fffb3eb7bbeffaa8c899a0fe7559aec8cc622bb3
brcjk2 zh <jiaxi.zhang@g.northernacademy.org>
2023-04-16 17:36:40 +0000

    Revert "Revert "maybe final;""
    This reverts commit b676fa758c44db5a9b3c03d5e7a9b1fa80f29b1e.

--- vote.sh
+++ vote.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-CWD=$(cd $(dirname $0) && pwd)
+CWD=$(cd $(dirname "$0") && pwd)
+
+set -o noglob
 
 NUM=$1
 AGR="$@"
@@ -8,11 +10,11 @@ AGR="$@"
 while getopts ":-:" opt; do
   case $opt in
     -)
-          echo "Usage: ./vote.sh VAL|\"None\" [[-i|--input-file FILE]|LIST]..."
+          echo "Usage: ./vote.sh VAL|\"None\" [[-i|--input-file FILE]|LIST...]"
           exit 0
 ;;
     \?)
-          echo "Usage: ./vote.sh VAL|\"None\" [[-i|--input-file FILE]|LIST]..."
+          echo "Usage: ./vote.sh VAL|\"None\" [[-i|--input-file FILE]|LIST...]"
           exit 0
       ;;  
   esac
@@ -35,12 +37,11 @@ while getopts ":-:i:" opt; do
         fi
         file=$2
         shift 2
-        echo $0
-        /usr/bin/python3 $CWD/vote.py $NUM $(cat $file) "$@"
+        /usr/bin/python3 $CWD/vote.py "$NUM" $(<"$file") "$@"
         exit 0
         
       else 
-      echo "Usage: ./vote.sh VAL|\"None\" [[-i|--input-file FILE]|LIST]..."; exit 0 
+      echo "Usage: ./vote.sh VAL|\"None\" [[-i|--input-file FILE]|LIST...]"; exit 0 
       fi
       ;;
     -)
@@ -55,11 +56,10 @@ while getopts ":-:i:" opt; do
             exit 2
           fi
           shift 2
-          /usr/bin/python3 $CWD/vote.py $NUM $(cat $val) "$@"
+          /usr/bin/python3 $CWD/vote.py "$NUM" $(<"$val") "$@"
           exit 0
       ;;
         *)
-<<<<<<< HEAD
           echo "Usage: ./vote.sh VAL|\"None\" [[-i|--input-file FILE]|LIST...]"
           
       ;;
@@ -67,15 +67,6 @@ while getopts ":-:i:" opt; do
     \?)
           echo "Usage: ./vote.sh VAL|\"None\" [[-i|--input-file FILE]|LIST...]"
           
-=======
-          echo "Usage: ./vote.sh VAL|\"None\" [[-i|--input-file FILE]|LIST]..."
-          exit 0
-      ;;
-      esac;;
-    \?)
-          echo "Usage: ./vote.sh VAL|\"None\" [[-i|--input-file FILE]|LIST]..."
-          exit 0
->>>>>>> parent of 468e202 (maybe final;)
       ;;
     :)
       echo "File not provided"
